c4328c355b542eea090184b88c90b13b45983221
Roitxelo <roiblancolage@gmail.com>
2026-09-17 10:34:19 +0200

    Avance ejercicio 2

--- UD1_T01/tarea2.py
+++ UD1_T01/tarea2.py
@@ -1,2 +1,14 @@
 listaNum = input(print("Escribe un lista números separados por espacios."))
 
+arrayPares, arrayImpares = array('i')
+cPares, cImpares = 0
+arrayNum = listaNum.split(" ")
+
+for num in arrayNum:
+    if num % 2 == 0:
+        arrayPares[cPares] = num
+        cPares + 1
+    else:
+        arrayImpares[cImpares] = num
+        cImpares + 1
+        
